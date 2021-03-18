559767eec0623f092b2020bd2d8e8028a6033f04
10B-45130 <58695976+10B-45130@users.noreply.github.com>
2021-03-18 09:17:02 +0100

    Add files via upload

---
+++ PROJEKT/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+        require './steamauth/steamauth.php'; 
+
+        if(isset($_SESSION['steamid'])) {
+            include ('steamauth/userInfo.php');
+        }   
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/bcbbe0b4e9.js" crossorigin="anonymous"></script>
+    <title>IPZ - 2021</title>
+</head>
+<body>
+
+
+
+    <header></header>
+        <div class="on-header">
+            <div class="belt">
+                <h2>Just find your team </h2>                
+            </div>    
+        </div>
+
+        <div class="belt-up">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6 left">
+                        <h1>LOGO - IPZ</h1>
+                    </div>
+                    <div class="col-6 right">                           
+                        
+                    <?php
+                        if(isset($_SESSION['steamid'])) {
+                            ?>
+                            <div class="log">
+                                <p><img src="<?php echo $steamprofile['avatar'];?>" alt="">     <?php echo $steamprofile['personaname'];?></p>
+                               <?php 
+                               logoutbutton(); 
+                               ?> 
+                        </div>
+                    <?php
+                        }  else {
+                            loginbutton();
+                        }   
+                    ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="belt-down">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6 right-down">
+
+                    </div>
+                    <div class="col-6 left-down">
+                        <span>
+                            <i class="fab fa-facebook-square"></i>                            
+                            <i class="fab fa-instagram"></i>
+                        </span>                        
+                    </div>
+                </div>
+            </div>           
+        </div>
+    
+</body>
+</html>
